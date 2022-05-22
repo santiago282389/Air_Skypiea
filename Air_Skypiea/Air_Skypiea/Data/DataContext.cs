076f0827fda325de00f09cee4d7fad009d7c51bb
santiago282389 <santi-arias-@outlook.com>
2022-05-21 21:00:43 -0500

    finish DataContext

---
+++ Air_Skypiea/Air_Skypiea/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Air_Skypiea.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Air_Skypiea.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
---
+++ Air_Skypiea/Air_Skypiea/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Air_Skypiea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
