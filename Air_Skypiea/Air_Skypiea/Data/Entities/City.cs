2684e32dfe1df4605358fdc9331af7deccaf809c
santiago282389 <santi-arias-@outlook.com>
2022-05-24 13:55:09 -0500

    finish register user

--- Air_Skypiea/Air_Skypiea/Data/Entities/City.cs
+++ Air_Skypiea/Air_Skypiea/Data/Entities/City.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Air_Skypiea.Data.Entities
 {
@@ -11,7 +12,7 @@ namespace Air_Skypiea.Data.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
 
-        //[JsonIgnore]
+        [JsonIgnore]
         public State State { get; set; }
 
         public ICollection<User> Users { get; set; }
---
+++ Air_Skypiea/Air_Skypiea/Helpers/IBlobHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Air_Skypiea.Helpers
+{
+    public interface IBlobHelper
+    {
+        Task<Guid> UploadBlobAsync(IFormFile file, string containerName);
+
+        Task<Guid> UploadBlobAsync(byte[] file, string containerName);
+
+        Task<Guid> UploadBlobAsync(string image, string containerName);
+
+        Task DeleteBlobAsync(Guid id, string containerName);
+    }
+}
