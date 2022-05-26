1c6a56a8f7c9bdd9023aedb2266bb2d2e3f70214
santiago282389 <santi-arias-@outlook.com>
2022-05-26 17:57:34 -0500

    Basic home screen

--- Air_Skypiea/Air_Skypiea/Controllers/HomeController.cs
+++ Air_Skypiea/Air_Skypiea/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Air_Skypiea.Models;
+﻿using Air_Skypiea.Data;
+using Air_Skypiea.Data.Entities;
+using Air_Skypiea.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Air_Skypiea.Controllers
@@ -7,16 +10,50 @@ namespace Air_Skypiea.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Product>? products = await _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.ProductCategories)
+                .OrderBy(p => p.Description)
+                .ToListAsync();
+
+            List<ProductsHomeViewModel> productsHome = new() { new ProductsHomeViewModel() };
+            int i = 1;
+            foreach (Product? product in products)
+            {
+                if (i == 1)
+                {
+                    productsHome.LastOrDefault().Product1 = product;
+                }
+                if (i == 2)
+                {
+                    productsHome.LastOrDefault().Product2 = product;
+                }
+                if (i == 3)
+                {
+                    productsHome.LastOrDefault().Product3 = product;
+                }
+                if (i == 4)
+                {
+                    productsHome.LastOrDefault().Product4 = product;
+                    productsHome.Add(new ProductsHomeViewModel());
+                    i = 0;
+                }
+                i++;
+            }
+
+            return View(productsHome);
         }
+
 
         public IActionResult Privacy()
         {
---
+++ Air_Skypiea/Air_Skypiea/Models/ProductsHomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Air_Skypiea.Data.Entities;
+
+namespace Air_Skypiea.Models
+{
+    public class ProductsHomeViewModel
+    {
+        public Product Product1 { get; set; }
+
+        public Product Product2 { get; set; }
+
+        public Product Product3 { get; set; }
+
+        public Product Product4 { get; set; }
+    }
+
+}
